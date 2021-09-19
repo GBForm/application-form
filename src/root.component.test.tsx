07bec2aa157e1ee089958437295dbd70e2310831
Sam Carbine <scarbine@me.com>
2021-09-19 12:49:02 -0500

    added a jest test to make sure the header Construction Loan Application renders in the document

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,6 +1,7 @@
 import { ExpansionPanelActions } from "@material-ui/core";
 import { render } from "@testing-library/react";
 import { iterator } from "core-js/library/es6/symbol";
+import { ConstructionLoanApplicationForm } from "./components/ConstructionLoanApplicationForm.tsx/ConstructionLoanApplicationForm";
 import Root from "./root.component";
 
 describe("Root component", () => {
@@ -13,12 +14,12 @@ describe("Root component", () => {
 // This test is making sure that the form properly capotured and set the value of firstName in local storage to a not null or undefined value
 // Currently this test
 describe("Loan Appliaction Component", () => {
-  it("should have a local storage value for firstName not be null", () => {
-    const firstName = localStorage.getItem("firstName");
-    expect(firstName).toBe(!null);
-  });
-  it("should have a local storage value for firstName not be undefined", () => {
-    const firstName = localStorage.getItem("firstName");
-    expect(firstName).toBe(!undefined);
+  // it("should have a local storage value for firstName not be null", () => {
+  //   const firstName = localStorage.getItem("firstName");
+  //   expect(firstName).toBe(!null);
+  // });
+  it("should be in the document", () => {
+    const { getByText } = render(<ConstructionLoanApplicationForm />);
+    expect(getByText(/Construction Loan Application/i)).toBeInTheDocument();
   });
 });
